4ce04868c8bbcf5022075d238b38590fd4ad7c42
sundeep kumar <sundeepkr007@gmail.com>
2023-04-04 12:32:11 +0530

    First commit

---
+++ src/test/java/Test01.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class Test01 {
+
+	@Test
+	public void method1()
+	{
+		System.out.println("----method1----");
+	}
+	
+
+	@Test
+	public void method2()
+	{
+		System.out.println("----method1----");
+	}
+}
---
+++ src/test/java/Test02.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class Test02 {
+
+	@Test
+	public void method3()
+	{
+		System.out.println("----method3----");
+	}
+	
+}
